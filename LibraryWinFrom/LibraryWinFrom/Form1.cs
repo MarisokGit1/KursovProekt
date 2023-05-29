95fb68fd3553d3665a30aed1b36d4371a2964d6c
MarisokGit1 <robloxmarisok@gmail.com>
2023-05-29 12:48:18 +0300

    changes

--- LibraryWinFrom/LibraryWinFrom/Form1.cs
+++ LibraryWinFrom/LibraryWinFrom/Form1.cs
@@ -36,5 +36,15 @@ namespace LibraryWinFrom
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
