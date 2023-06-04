0612ae178ead57baea87f688faac05c99ecc3c32
MarisokGit <robloxmarisok@gmail.com>
2023-06-04 11:41:51 +0300

    h

--- LibraryWinforms/LibraryWinforms/Form1.Designer.cs
+++ LibraryWinforms/LibraryWinforms/Form1.Designer.cs
@@ -29,37 +29,98 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            button4 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::LibraryWinforms.Properties.Resources.download1;
-            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(135, 125);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            pictureBox1.Image = Properties.Resources.download1;
+            pictureBox1.Location = new Point(300, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(135, 125);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.Location = new Point(216, 140);
+            label1.Name = "label1";
+            label1.Size = new Size(366, 35);
+            label1.TabIndex = 1;
+            label1.Text = "Welcome to Marin's Library ";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Showcard Gothic", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(273, 175);
+            label2.Name = "label2";
+            label2.Size = new Size(211, 18);
+            label2.TabIndex = 2;
+            label2.Text = "State your business here";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(273, 228);
+            button1.Name = "button1";
+            button1.Size = new Size(211, 62);
+            button1.TabIndex = 3;
+            button1.Text = "Register a book";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(273, 296);
+            button4.Name = "button4";
+            button4.Size = new Size(211, 62);
+            button4.TabIndex = 6;
+            button4.Text = "Issue a book";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(273, 364);
+            button2.Name = "button2";
+            button2.Size = new Size(211, 62);
+            button2.TabIndex = 7;
+            button2.Text = "Return a book";
+            button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.RosyBrown;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox1);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
-            this.Text = "Library";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-
+            AutoScaleMode = AutoScaleMode.None;
+            BackColor = Color.RosyBrown;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button4);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Font = new Font("Showcard Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Library";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Button button4;
+        private Button button2;
     }
 }
--- LibraryWinforms/LibraryWinforms/Form1.cs
+++ LibraryWinforms/LibraryWinforms/Form1.cs
@@ -11,5 +11,10 @@ namespace LibraryWinforms
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
